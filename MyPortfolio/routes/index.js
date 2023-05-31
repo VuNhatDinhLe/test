050686b93a0be9b224566bc43e91e5bf61966adf
VuNhatDinhLe <aafterschool11@gmail.com>
2023-05-31 01:40:14 -0400

    commit

--- MyPortfolio/routes/index.js
+++ MyPortfolio/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about_me', function(req, res, next) {
+  res.render('about_me', { title: 'Express' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Express' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Express' });
+}); 
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
