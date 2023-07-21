505e00c9301e66076d507fe71069551d9e8bc621
VuNhatDinhLe <aafterschool11@gmail.com>
2023-07-21 13:09:35 -0400

    cm

--- WEEK41/Product/app/config/db.config.js
+++ WEEK41/Product/app/config/db.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
    //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
 
-//url:"mongodb+srv://Blessing:KF4CzZpj8tlYGtap@cluster0.29680fe.mongodb.net/SportStore?retryWrites=true&w=majority"
-url:"mongodb+srv://aafterschool11:Cabasa123@cluster0.ar24enw.mongodb.net/DressStore?retryWrites=true&w=majority"
+url:"mongodb+srv://Blessing:KF4CzZpj8tlYGtap@cluster0.29680fe.mongodb.net/SportStore?retryWrites=true&w=majority"
+//url:"mongodb+srv://aafterschool11:Cabasa123@cluster0.ar24enw.mongodb.net/DressStore?retryWrites=true&w=majority"
 
 };
