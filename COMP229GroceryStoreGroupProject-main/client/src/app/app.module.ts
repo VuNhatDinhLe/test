6ac95c1ca061ec650c2d8e784701f4aa8c94e0e7
VuNhatDinhLe <133162067+VuNhatDinhLe@users.noreply.github.com>
2023-07-24 22:08:55 -0400

    cm

--- COMP229GroceryStoreGroupProject-main/client/src/app/app.module.ts
+++ COMP229GroceryStoreGroupProject-main/client/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ImageComponent } from './components/image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
     ProductDetailsComponent,
+    ImageComponent,
     ProductsListComponent
   ],
   imports: [
---
+++ COMP229GroceryStoreGroupProject-main/client/src/app/models/image.model.ts
@@ -0,0 +1,14 @@
+//import { ObjectId } from "mongoose";
+import { Buffer } from 'buffer/';
+
+export class Image {
+
+    id?: any;
+    name?: string;
+    description?: string;
+    
+        data?: Buffer;
+        contentType?: String;
+    
+  }
+  
