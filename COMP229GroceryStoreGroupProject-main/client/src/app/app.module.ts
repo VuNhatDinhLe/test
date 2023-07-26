56e178d96a8a28d5945e033ed36ff684564a38c0
VuNhatDinhLe <aafterschool11@gmail.com>
2023-07-25 23:49:10 -0400

    cm

--- COMP229GroceryStoreGroupProject-main/client/src/app/app.module.ts
+++ COMP229GroceryStoreGroupProject-main/client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddProductComponent } from './components/add-product/add-product.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ImageComponent } from './components/image/image.component';
@@ -14,7 +13,6 @@ import { ImageComponent } from './components/image/image.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent,
     ProductDetailsComponent,
     ImageComponent,
     ProductsListComponent
